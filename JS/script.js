0ed337d2458d29838836fd4a44a754935c7cbe8b
deb <debkbanerji@gmail.com>
2016-05-18 18:56:52 -0400

    Scrolling Banner
    Started work on scrolling banner - basic logic working

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,27 @@
 // alert("Test");
+
+//Scrolling logic for banner
+
+// Create cross browser requestAnimationFrame method:
+window.requestAnimationFrame = window.requestAnimationFrame
+    || window.mozRequestAnimationFrame
+    || window.webkitRequestAnimationFrame
+    || window.msRequestAnimationFrame
+    || function (f) {
+        setTimeout(f, 1000 / 60)
+    };
+
+var bannerImage = document.getElementById('main-banner-image');
+
+function scrollBanner() {
+    var scrollOffset = window.pageYOffset; //store number of pixels the document has scrolled down
+    var backgroundPosition = '50% ' + (scrollOffset * -0.5) + 'px';
+    bannerImage.style.backgroundPosition = backgroundPosition;
+    console.log(backgroundPosition);
+
+}
+
+window.addEventListener('scroll', function () { // on page scroll
+    requestAnimationFrame(scrollBanner); // call parallaxbubbles() on next available screen paint
+    console.log('scrolling' + window.pageYOffset);
+}, false);
