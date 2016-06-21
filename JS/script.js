dc2c9a072b735db41aca3a5dde33799d2879580d
Deb Banerji <debkbanerji@gmail.com>
2016-06-21 18:04:33 +0100

    Updated CSS loading in Javascript

--- JS/script.js
+++ JS/script.js
@@ -118,9 +118,39 @@ $('.carousel').carousel({
     }
 }( typeof global !== "undefined" ? global : this ));
 
-loadCSS("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css");
-try{
-    loadCSS("./CSS/style.css");
-} catch (e) {
-    loadCSS("../CSS/style.css");
+function onloadCSS( ss, callback ) {
+    var called;
+    function newcb(){
+        if( !called && callback ){
+            called = true;
+            callback.call( ss );
+        }
+    }
+    if( ss.addEventListener ){
+        ss.addEventListener( "load", newcb );
+    }
+    if( ss.attachEvent ){
+        ss.attachEvent( "onload", newcb );
+    }
+
+    // This code is for browsers that don’t support onload
+    // No support for onload (it'll bind but never fire):
+    //	* Android 4.3 (Samsung Galaxy S4, Browserstack)
+    //	* Android 4.2 Browser (Samsung Galaxy SIII Mini GT-I8200L)
+    //	* Android 2.3 (Pantech Burst P9070)
+
+    // Weak inference targets Android < 4.4
+    if( "isApplicationInstalled" in navigator && "onloadcssdefined" in ss ) {
+        ss.onloadcssdefined( newcb );
+    }
 }
+
+var stylesheetBootstrap = loadCSS( "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css");
+onloadCSS( stylesheetBootstrap, function() {
+    console.log( "Stylesheet bootstrap has loaded." );
+});
+
+var stylesheetStyle = loadCSS("./CSS/style.css");
+onloadCSS( stylesheetStyle, function() {
+    console.log("Stylesheet style has loaded.");
+});
